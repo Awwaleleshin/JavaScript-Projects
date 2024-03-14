444fc509d930ebed1d42c9a816de8c9faa51b437
Eleshin <awwaleleshin@gmail.com>
2024-03-14 09:28:13 +0100

    remove and toggle active classList

--- FAQs/script.js
+++ FAQs/script.js
@@ -2,6 +2,14 @@ const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
-        faq.classList.toggle('active')
-    })
+        // Remove 'active' class from all FAQs
+        faqs.forEach(item => {
+            if (item !== faq) {
+                item.classList.remove('active');
+            }
+        });
+
+        // Toggle 'active' class for the clicked FAQ
+        faq.classList.toggle('active');
+    });
 });
